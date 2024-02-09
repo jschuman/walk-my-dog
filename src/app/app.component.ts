b5515335ef24d921778d62ce2ffa88dbdb3b7791
Jeff Schuman <jschuman@solutionstreet.com>
2024-02-09 15:46:25 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,44 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <nav>
+      <div>
+        <a [routerLink]="['/']"><img class="logo" src="assets/dog-walker-logo.svg"></a>
+        <h1 class="title">Walk My Dog</h1>
+      </div>
+    </nav>
     <router-outlet></router-outlet>
   `,
-  styles: [],
+  styles: [
+    `
+  nav {
+      display: flex;
+      justify-content: space-between;
+      margin: 20px 10px 10px 10px;
+    }
+    
+    nav div.menu-items {
+      display: flex;
+      width: 10%;
+      justify-content: space-between;
+    }
+    nav > div {
+      display: flex;
+    }
+    .title {
+      font-size: 25pt;
+    }
+    .logo {
+      height: 40px;
+    }
+  `
+  ],
 })
 export class AppComponent {
   title = 'walk-my-dog';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DogsListComponent } from './dogs-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'list'},
+  {path: 'list', component: DogsListComponent},
+  {path: 'details/:index', loadComponent: () => import('./dog-view.component').then(m=>m.DogViewComponent) }
+];
